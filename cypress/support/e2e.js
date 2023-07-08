d9be66d9955a9c94b7f4d1d65d4858f453a5f86f
Murat Ozcan <muratkerem@gmail.com>
2023-07-08 14:06:03 -0500

    working [ci-skip]

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -2,12 +2,6 @@ import './commands'
 import 'cypress-mailosaur'
 import 'cypress-data-session'
 
-/**
- * Parses a confirmation code from a given string.
- *
- * @param {string} str - The string to parse.
- * @returns {(string|null)} The parsed confirmation code or null if no code is found.
- */
 const parseConfirmationCode = str => {
   const regex = /Your confirmation code is (\w+)/
   const match = str.match(regex)
@@ -15,31 +9,27 @@ const parseConfirmationCode = str => {
   return match ? match[1] : null
 }
 
-/**
- * Retrieves an email message and extracts the confirmation code.
- *
- * @param {string} userEmail - The email address to which the message was sent.
- * @returns {(string|null)} The extracted confirmation code or null if no code is found.
- */
 const getConfirmationCode = userEmail => {
   return cy
     .mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVERID'), {
       sentTo: userEmail,
     })
+    .should(Cypress._.noop)
     .its('html.body')
     .should('be.a', 'string')
     .then(parseConfirmationCode)
 }
 
-/**
- * Registers a new user, retrieves and enters the confirmation code, and signs the user in.
- *
- * @param {Object} param0 - An object containing the user's full name, username, email, and password.
- * @param {string} param0.fullName - The user's full name.
- * @param {string} param0.userName - The user's username.
- * @param {string} param0.email - The user's email address.
- * @param {string} param0.password - The user's password.
- */
+const confirmRegistration = email => {
+  return getConfirmationCode(email).then(code => {
+    cy.get('#verification-code').type(code, {delay: 0})
+    cy.contains('button', 'Confirm registration').click()
+    cy.wait('@cognito')
+    cy.contains('You are now registered!').should('be.visible')
+    cy.contains('button', /ok/i).click()
+  })
+}
+
 const registerUserOnce = ({fullName, userName, email, password}) => {
   const firstName = fullName.split(' ')[0]
   const lastName = fullName.split(' ')[1]
@@ -56,14 +46,7 @@ const registerUserOnce = ({fullName, userName, email, password}) => {
   cy.contains('button', 'Create an account').click()
   cy.wait('@cognito').its('response.statusCode').should('equal', 200)
 
-  return getConfirmationCode(email).then(code => {
-    cy.get('#verification-code').type(code, {delay: 0})
-    cy.contains('button', 'Confirm registration').click()
-    cy.wait('@cognito')
-    cy.contains('You are now registered!').should('be.visible')
-    cy.contains('button', /ok/i).click()
-    return cy.wrap(userName) // if the user gets registered, we return their email address
-  })
+  confirmRegistration(email)
 }
 
 const signIn = ({userName, password}) => {
@@ -78,9 +61,10 @@ const signIn = ({userName, password}) => {
 const registerUser = ({fullName, userName, email, password}) =>
   cy.dataSession({
     name: email, // unique name of the data session will be the email address
-    setup: () => registerUserOnce({fullName, userName, email, password}), // yields the registered user's email address to validate and recreate as an argument
-    validate: registeredEmail => registeredEmail === email, // if the email address is the same as the registered user's email address, the user is valid, otherwise run setup again
-    recreate: () => signIn({userName, password}), // if the user is valid/registered, just sign in with them
+    init: () => registerUserOnce({fullName, userName, email, password}), // only runs initially
+    setup: () => confirmRegistration(email), //
+    validate: confirmationCode => Boolean(confirmationCode), //
+    recreate: () => signIn({userName, password}),
     cacheAcrossSpecs: true,
   })
 Cypress.Commands.add('registerUser', registerUser)
