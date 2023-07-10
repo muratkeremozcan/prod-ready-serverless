e54ce45e970d3493a31c7581d8e337b3da8339da
Murat Ozcan <muratkerem@gmail.com>
2023-07-10 10:13:54 -0500

    added db seed check

--- __tests__/setup/seed-restaurants.js
+++ __tests__/setup/seed-restaurants.js
@@ -1,55 +1,66 @@
 const {DynamoDB} = require('@aws-sdk/client-dynamodb')
 const {marshall} = require('@aws-sdk/util-dynamodb')
+const dynamodb = new DynamoDB({
+  region: 'us-east-1',
+})
 require('dotenv').config()
 
+const restaurants = [
+  {
+    name: 'Fangtasia',
+    image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/fangtasia.png',
+    themes: ['true blood'],
+  },
+  {
+    name: "Shoney's",
+    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/shoney's.png",
+    themes: ['cartoon', 'rick and morty'],
+  },
+  {
+    name: "Freddy's BBQ Joint",
+    image:
+      "https://d2qt42rcwzspd6.cloudfront.net/manning/freddy's+bbq+joint.png",
+    themes: ['netflix', 'house of cards'],
+  },
+  {
+    name: 'Pizza Planet',
+    image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/pizza+planet.png',
+    themes: ['netflix', 'toy story'],
+  },
+  {
+    name: 'Leaky Cauldron',
+    image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/leaky+cauldron.png',
+    themes: ['movie', 'harry potter'],
+  },
+  {
+    name: "Lil' Bits",
+    image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/lil+bits.png',
+    themes: ['cartoon', 'rick and morty'],
+  },
+  {
+    name: 'Fancy Eats',
+    image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/fancy+eats.png',
+    themes: ['cartoon', 'rick and morty'],
+  },
+  {
+    name: 'Don Cuco',
+    image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/don%20cuco.png',
+    themes: ['cartoon', 'rick and morty'],
+  },
+]
+
 const seedDatabase = async () => {
-  const dynamodb = new DynamoDB({
-    region: 'us-east-1',
+  // Check if the database is already seeded
+  const data = await dynamodb.scan({
+    TableName: process.env.restaurants_table,
+    Limit: 1,
   })
 
-  const restaurants = [
-    {
-      name: 'Fangtasia',
-      image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/fangtasia.png',
-      themes: ['true blood'],
-    },
-    {
-      name: "Shoney's",
-      image: "https://d2qt42rcwzspd6.cloudfront.net/manning/shoney's.png",
-      themes: ['cartoon', 'rick and morty'],
-    },
-    {
-      name: "Freddy's BBQ Joint",
-      image:
-        "https://d2qt42rcwzspd6.cloudfront.net/manning/freddy's+bbq+joint.png",
-      themes: ['netflix', 'house of cards'],
-    },
-    {
-      name: 'Pizza Planet',
-      image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/pizza+planet.png',
-      themes: ['netflix', 'toy story'],
-    },
-    {
-      name: 'Leaky Cauldron',
-      image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/leaky+cauldron.png',
-      themes: ['movie', 'harry potter'],
-    },
-    {
-      name: "Lil' Bits",
-      image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/lil+bits.png',
-      themes: ['cartoon', 'rick and morty'],
-    },
-    {
-      name: 'Fancy Eats',
-      image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/fancy+eats.png',
-      themes: ['cartoon', 'rick and morty'],
-    },
-    {
-      name: 'Don Cuco',
-      image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/don%20cuco.png',
-      themes: ['cartoon', 'rick and morty'],
-    },
-  ]
+  // If data already exists in the table, return early
+  if (data.Items && data.Items.length > 0) {
+    console.log('Database is already seeded')
+    return
+  }
 
   const putReqs = restaurants.map(x => ({
     PutRequest: {
