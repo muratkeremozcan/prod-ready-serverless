70b325e9b05c20654704da1d6e57faec209fe45e
Murat Ozcan <muratkerem@gmail.com>
2023-07-12 10:37:32 -0500

    used ssm instead of env vars

--- functions/search-restaurants.js
+++ functions/search-restaurants.js
@@ -1,9 +1,14 @@
 const {DynamoDB} = require('@aws-sdk/client-dynamodb')
 const {marshall, unmarshall} = require('@aws-sdk/util-dynamodb')
+const middy = require('@middy/core')
+const ssm = require('@middy/ssm')
 const dynamodb = new DynamoDB()
-
-const defaultResults = process.env.defaultResults || 8
+const {serviceName, ssmStage} = process.env
 const tableName = process.env.restaurants_table
+// We need to parse the two new environment variables
+// because all environment variables would come in as strings
+const middyCacheEnabled = JSON.parse(process.env.middy_cache_enabled)
+const middyCacheExpiry = parseInt(process.env.middy_cache_expiry_milliseconds)
 
 const findRestaurantsByTheme = async (theme, count) => {
   console.log(`finding (up to ${count}) restaurants with the theme ${theme}...`)
@@ -23,13 +28,26 @@ const findRestaurantsByTheme = async (theme, count) => {
   }
 }
 
-module.exports.handler = async event => {
+// Load app configurations from SSM Parameter Store with cache and cache invalidation
+module.exports.handler = middy(async (event, context) => {
   const {theme} = JSON.parse(event.body)
-  const restaurants = await findRestaurantsByTheme(theme, defaultResults)
+  const restaurants = await findRestaurantsByTheme(
+    theme,
+    context.config.defaultResults,
+  )
   const response = {
     statusCode: 200,
     body: JSON.stringify(restaurants),
   }
 
   return response
-}
+}).use(
+  ssm({
+    cache: middyCacheEnabled,
+    cacheExpiry: middyCacheExpiry,
+    setToContext: true,
+    fetchData: {
+      config: `/${serviceName}/${ssmStage}/search-restaurants/config`,
+    },
+  }),
+)
