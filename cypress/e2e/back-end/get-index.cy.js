71ec3a8efb1f8136a01a420a6ec5eb99b88c6377
Murat Ozcan <muratkerem@gmail.com>
2023-07-29 08:27:51 -0500

    part 3 reviewed test skip [ci skip]

--- cypress/e2e/back-end/get-index.cy.js
+++ cypress/e2e/back-end/get-index.cy.js
@@ -1,16 +1,14 @@
 import spok from 'cy-spok'
 describe(`When we invoke the GET / endpoint`, () => {
   it(`Should return the index page with 8 restaurants`, () => {
-    cy.viaHttp('', 'GET')
-      .should(
-        spok({
-          status: 200,
-          headers: {
-            'content-type': 'text/html; charset=UTF-8',
-          },
-          body: spok.string,
-        }),
-      )
-      .its('body')
+    cy.viaHttp('', 'GET').should(
+      spok({
+        status: 200,
+        headers: {
+          'content-type': 'text/html; charset=UTF-8',
+        },
+        body: spok.string,
+      }),
+    )
   })
 })
