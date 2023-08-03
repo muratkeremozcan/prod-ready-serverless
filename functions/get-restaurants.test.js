bde918f918788e424966a975738d48126d17c8e6
Murat Ozcan <muratkerem@gmail.com>
2023-08-03 14:12:24 -0500

    log for Nicholas

--- functions/get-restaurants.test.js
+++ functions/get-restaurants.test.js
@@ -4,8 +4,8 @@ describe(`When we invoke the GET /restaurants endpoint`, () => {
   it(`Should return an array of 8 restaurants`, async () => {
     const res = await when.we_invoke_get_restaurants()
 
-    expect(res.statusCode).toEqual(200)
     console.log({response: res})
+    expect(res.statusCode).toEqual(200)
     expect(res.body).toHaveLength(8)
 
     for (let restaurant of res.body) {
